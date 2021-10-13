28d643b7b4e13f8de6649dfb6715ef3fb4e4733e
Qabas al ani <doctorqa@yahoo.com>
2021-10-13 12:00:01 -0400

    installed the type react to manage the typography and the speed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,17 @@
-import React from 'react'
-import { Typography, Avatar, Grid, Box } from '@material-ui/core'
-import avatar from " ../avatar.jpg"
+import React from "react";
+import { Typography, Avatar, Grid, Box } from "@material-ui/core";
+import Typed from "react-typed";
+import avatar from "../avatar.jpg";
 
 const Header = () => {
-    return (
-        <Box>
-            <Avatar src={avatar} alt="" />
-        </Box>
-    )
-}
+  return (
+    <Box>
+      <Typography variant="h4">
+        <Typed strings={["Qabas Al Ani"]} typeSpeed={90} />
+      </Typography>
+      <Avatar src={avatar} alt="Qabas Al Ani" />
+    </Box>
+  );
+};
 
-export default Header
+export default Header;
