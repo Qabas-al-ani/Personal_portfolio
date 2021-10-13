1a4dd991bfdab87bbd99abed7ecedf2ac189e7f0
Qabas al ani <doctorqa@yahoo.com>
2021-10-13 17:53:36 -0400

    imported makestyles typography and box to style the working experience section

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,10 +1,32 @@
 import React from 'react'
+import {makeStyles} from "@material-ui/core/styles"
+import { Typography, Box} from "@material-ui/core"
+import Navbar from "./Navbar"
+
+
+const useStyles = makeStyles({
+    mainContainer: {
+        background: "white"
+    }
+})
+
+
 
 const Resume = () => {
+    const classes = useStyles()
     return (
-        <div>
-            Hello world
-        </div>
+        <>
+        <Navbar />
+<Box component="header" className={classes.mainContainer}>
+<Typography variant="h4" align="center">
+    Working Experience 
+</Typography>
+<Box component="div">
+    <Typography variant="h2">2021</Typography>
+</Box>
+</Box>
+       
+        </>
     )
 }
 
