2bbb02b631c853f830fb672bc6bd43ceaf6e3563
Qabas al ani <doctorqa@yahoo.com>
2021-10-14 17:08:10 -0400

    created contact component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Resume from "./components/Resume";
 import {Route} from "react-router-dom"
 import Projects from "./components/Projects"
+import Contacts from "./components/Contacts"
 import "./App.css";
 
 
@@ -14,6 +15,7 @@ function App() {
       <Route exact path="/" component={Home} />
       <Route path="/resume" component={Resume} />
       <Route path="/projects" component={Projects} />
+      <Route path="/contacts" component={Contacts} />
    
     
     </>
---
+++ src/components/Contacts.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import { TextField, Typography, Button, Grid, Box } from '@material-ui/core'
+import SendIcon from '@material-ui/icons/Send'
+import Navbar from "./Navbar"
+
+const Contacts = () => {
+    return (
+        <div>
+            jkjkjkjkjkj
+        </div>
+    )
+}
+
+export default Contacts;
