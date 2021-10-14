2439821b4b7ed4530dc17bf23b83e965659cf52e
Qabas al ani <doctorqa@yahoo.com>
2021-10-13 22:02:03 -0400

    added another style to the resume section that can be seen in typography in resume.js in the className as the time line year

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,33 +1,79 @@
-import React from 'react'
-import {makeStyles} from "@material-ui/core/styles"
-import { Typography, Box} from "@material-ui/core"
-import Navbar from "./Navbar"
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Typography, Box } from "@material-ui/core";
+import Navbar from "./Navbar";
 
-
-const useStyles = makeStyles({
-    mainContainer: {
-        background: "white"
-    }
-})
-
-
+const useStyles = makeStyles(theme => ({
+  mainContainer: {
+    background: "white",
+  },
+  timeLine: {
+    position: "relative",
+    padding: "1rem",
+    margin: "0 auto",
+    "&:before": {
+      content: "''",
+      position: "absolute",
+      height: "100%",
+      border: "1px solid black",
+      right: "40px",
+      top: 0,
+    },
+    "&:after": {
+      content: "''",
+      display: "table",
+      clear: "both",
+    },
+    [theme.breakpoints.up("md")]: {
+      "&:before": {
+        left: "calc(50% - 1px)",
+        right: "auto",
+      },
+    },
+  },
+  timeLineYear: {
+    textAlign: "center",
+    maxWidth: "9.375rem",
+    margin: "0 3rem 0 auto",
+    fontSize: "1.8rem",
+    background: "tomato",
+    color: "white",
+    lineHeight: 1,
+    padding: "0.5rem 0 1rem",
+    "&:before": {
+      display: "none",
+    },
+    [theme.breakpoints.up("md")]: {
+      textAlign: "center",
+      margin: "0 auto",
+      "&:nth-of-type(2n)": {
+        float: "none",
+        margin: "0 auto"
+      },
+      "&:nth-of-type(2n):before": {
+        display: "none",
+      }
+    },
+  },
+}));
 
 const Resume = () => {
-    const classes = useStyles()
-    return (
-        <>
-        <Navbar />
-<Box component="header" className={classes.mainContainer}>
-<Typography variant="h4" align="center">
-    Working Experience 
-</Typography>
-<Box component="div">
-    <Typography variant="h2">2021</Typography>
-</Box>
-</Box>
-       
-        </>
-    )
-}
+  const classes = useStyles();
+  return (
+    <>
+      <Navbar />
+      <Box component="header" className={classes.mainContainer}>
+        <Typography variant="h4" align="center">
+          Working Experience
+        </Typography>
+        <Box component="div" className={classes.timeLine}>
+          <Typography variant="h2" className={classes.timeLineYear}>
+            2021
+          </Typography>
+        </Box>
+      </Box>
+    </>
+  );
+};
 
-export default Resume
+export default Resume;
