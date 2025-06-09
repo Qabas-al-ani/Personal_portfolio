3022aeaa3f4dc677871c7d3b4df047fde4be0b01
Qabas <qabasalani1@gmail.com>
2025-06-09 20:18:08 +0700

    Switch to HashRouter for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
+// import React from "react";
+// import Home from "./components/Home";
+// import CssBaseline from '@material-ui/core/CssBaseline';
+// import Resume from "./components/Resume";
+// import {Route} from "react-router-dom"
+// import Projects from "./components/Projects"
+// import Contacts from "./components/Contacts"
+// import AboutMe from "./components/AboutMe"
+// import "./App.css";
+
+// function App() {
+//   return (
+//     <>
+//       <CssBaseline />
+//       <Route exact path="/" component={Home} />
+//       <Route path="/about-me" component={AboutMe} />
+//       <Route path="/resume" component={Resume} />
+//       <Route path="/projects" component={Projects} />
+//       <Route path="/contacts" component={Contacts} />
+
+//     </>
+//   );
+// }
+
+// export default App;
+
 import React from "react";
 import Home from "./components/Home";
-import CssBaseline from '@material-ui/core/CssBaseline';
+import CssBaseline from "@material-ui/core/CssBaseline";
 import Resume from "./components/Resume";
-import {Route} from "react-router-dom"
-import Projects from "./components/Projects"
-import Contacts from "./components/Contacts"
-import AboutMe from "./components/AboutMe"
+import Projects from "./components/Projects";
+import Contacts from "./components/Contacts";
+import AboutMe from "./components/AboutMe";
+import { HashRouter as Router, Route } from "react-router-dom"; // ✅ use HashRouter
 import "./App.css";
-
 
 function App() {
   return (
-    <>
+    <Router>
+      {" "}
+      {/* ✅ Wrap all routes in HashRouter */}
       <CssBaseline />
-      <Route exact path="/" component={Home} />      
-      <Route path="/about-me" component={AboutMe} />       
+      <Route exact path="/" component={Home} />
+      <Route path="/about-me" component={AboutMe} />
       <Route path="/resume" component={Resume} />
       <Route path="/projects" component={Projects} />
       <Route path="/contacts" component={Contacts} />
-     
-    </>
+    </Router>
   );
 }
 
